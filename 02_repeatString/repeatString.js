8091ae88c49ce6e3854234c88834308d8b715f81
khalilar42 <khalilarchkak@gmail.com>
2024-02-12 20:28:37 +0100

    Completed exercise number 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (word, reps) {
+    if (reps < 0) {
+        return "ERROR";
+    }
 
+    let repeatedWord = "";
+    for (let index = 0; index < reps; index++) {
+        repeatedWord += word;
+    }
+
+    return repeatedWord;
 };
 
 // Do not edit below this line
