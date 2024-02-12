d70edb5ae9968068c5b8be5eae2f0911f21baa35
khalilar42 <khalilarchkak@gmail.com>
2024-02-12 20:38:15 +0100

    Completed exercise number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let reversedWord = "";
+    for (let index = word.length - 1; index >= 0; index--) {
+        reversedWord += word.charAt(index);
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
