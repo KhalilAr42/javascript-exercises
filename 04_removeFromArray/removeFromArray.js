d99b9700dd659b51f4478710acb3cb09177344e4
khalilar42 <khalilarchkak@gmail.com>
2024-02-13 08:12:25 +0100

    Completed exercise number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+    const result = array.filter((word) => elements.indexOf(word) == -1);
+    return result;
 };
 
 // Do not edit below this line
