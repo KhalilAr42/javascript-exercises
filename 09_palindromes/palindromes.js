82b0c4c3896e449a82ebc90830a875506f23e579
khalilar42 <khalilarchkak@gmail.com>
2024-02-24 15:56:29 +0100

    Complete exercise number 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordWithNoPunctuation = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    let trimmedWord = wordWithNoPunctuation.replace(/ /g, "").toLowerCase();
 
+    for (let i = 0; i < trimmedWord.length / 2; i++) {
+        if (trimmedWord[i] != trimmedWord[trimmedWord.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
