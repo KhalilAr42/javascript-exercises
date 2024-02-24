aaa44ec58b0bf61bd26de4fd1409ef08d9c2e25f
khalilar42 <khalilarchkak@gmail.com>
2024-02-24 16:12:10 +0100

    Complete exercise number 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number == 0) {
+        return 0;
+    }
 
+    if (number == 1) {
+        return 1;
+    }
+
+    previous = 1;
+    next = 1;
+
+    for (let i = 0; i < number - 2; i++) {
+        let temp = previous + next;
+        previous = next;
+        next = temp;
+    }
+    return next;
 };
 
 // Do not edit below this line
